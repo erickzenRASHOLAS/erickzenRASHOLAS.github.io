3d20ba313f175809cb6593842e9272c9b43fe8ad
erick parra <er.rosales@duocuc.cl>
2026-09-22 16:22:30 -0300

    Se agregar TODO el CV a react

---
+++ cv-webpage/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+        return(
+            <footer>
+                <p>
+
+                </p>
+            );
+    }
+
+export default Footer
--- readme.md
+++
@@ -1,1 +0,0 @@
-npx @tailwindcss/cli -i ./assets/src/style.css -o ./assets/css/style.css --watch
